e9f71b327cc2bb531fe817bd02fbd9769c994a3e
Littlestar0508 <rist9860@gmail.com>
2026-05-05 02:44:56 +0900

    모바일과 웹 화면에서의 홈 버튼 위치 차이 없애기

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import ErrorBoundary from './components/errorBoundary';
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
-      <main className="mx-auto min-h-screen w-full max-w-107.5 bg-white px-4 py-2">
+      <main className="mx-auto min-h-screen w-full max-w-107.5 bg-white px-4 py-2 relative">
         <h1 className="mb-4 text-4xl font-bold">AIPIA NEWS</h1>
 
         <ErrorBoundary>
--- src/components/moveToMainPage.tsx
+++ src/components/moveToMainPage.tsx
@@ -8,7 +8,7 @@ function MoveToMainPage() {
       <button
         type="button"
         onClick={() => navigate('/')}
-        className="w-12 fixed bottom-8 right-8 bg-blue-300 rounded-full p-2 z-999"
+        className="w-12 fixed bottom-8 right-[calc((100vw-430px)/2+32px)] bg-blue-300 rounded-full p-2 z-999 active:scale-95 transition"
       >
         <img src="/home.png" alt="메인 페이지로 이동" />
       </button>
