bd02f51af90cd778a4d0f2e314b5c68d45fe8264
Littlestar0508 <rist9860@gmail.com>
2026-05-05 02:37:00 +0900

    홈 버튼 css 일부 수정

--- src/components/moveToMainPage.tsx
+++ src/components/moveToMainPage.tsx
@@ -8,7 +8,7 @@ function MoveToMainPage() {
       <button
         type="button"
         onClick={() => navigate('/')}
-        className="w-12 absolute bottom-8 right-8 bg-blue-300 rounded-full p-2"
+        className="w-12 fixed bottom-8 right-8 bg-blue-300 rounded-full p-2 z-999"
       >
         <img src="/home.png" alt="메인 페이지로 이동" />
       </button>
